3c6328dcff983c988d53f1fad3498c14fe935726
Verity <v.a.r.spinks@se21.qmul.ac.uk>
2023-07-23 23:00:34 +0100

    removing file

--- Test file.py
+++ Test file.py
@@ -1,5 +1,25 @@
-import maya as maya
+#import maya as maya
+import nltk
+from nltk.corpus import stopwords
+sw_nltk = stopwords.words('english')
 
-print("hello")
-edited = maya
+#print("hello")
+#edited = maya
 
+textfile=open("trialpiece.txt", "rt") # opening the batchfile test and saving it to variable
+scannedtext=""
+for ll in textfile:
+    scannedtext+=ll.rstrip() # stripping the text of new lines
+
+strippedtext=scannedtext.replace("   ", " ") # removing the newlines to help with regex pattern
+
+textfile.close()
+#print(strippedtext)
+
+#words = [word for word in strippedtext.split() if word.lower() not in sw_nltk]
+#new_text = " ".join(words)
+#print(new_text)
+#print("Old length: ", len(strippedtext))
+#print("New length: ", len(new_text))
+
+print(sw_nltk)
